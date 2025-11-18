906e0f9561244b9d55a7b38d84fc00d480ab0401
baimeeb <74864183+baimeeb@users.noreply.github.com>
2025-11-18 14:37:33 +0800

    update

--- x6a_a64_ab/BoardConfig.mk
+++ x6a_a64_ab/BoardConfig.mk
@@ -1,4 +1,4 @@
 include device/phh/treble/board-base.mk
 include device/phh/treble/phhgsi_a64_ab/BoardConfig.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/x6a/sepolicy
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/x6a/sepolicy
